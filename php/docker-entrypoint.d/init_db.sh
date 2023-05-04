17b00b2140f1908fb5aa00f0aee9fe774db2ad12
Tim Fry <tim.fry@hotmail.com>
2023-05-04 17:32:51 -0300

    remove copy paste error

--- php/docker-entrypoint.d/init_db.sh
+++ php/docker-entrypoint.d/init_db.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/local/src/upgrade_apps/init_database.php
 if [ ! -f /var/www/fusionpbx/core/upgrade/init_database.php ]; then
     gosu fusionpbx sh -c "ln -s /usr/local/src/upgrade_apps/init_database.php /var/www/fusionpbx/core/upgrade/init_database.php"
 fi
