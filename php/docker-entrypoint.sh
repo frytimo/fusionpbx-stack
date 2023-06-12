164dd7214027a44920d541156797dc9429922b12
Tim Fry <tim.fry@hotmail.com>
2023-06-12 13:40:09 -0300

    reconfigure permissions script to have it's own file

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-if [ -d /var/log/freeswitch ]; then
-    chown -R fusionpbx:fusionpbx /var/log/freeswitch
-fi
-
-if [ -d /etc/freeswitch ]; then
-    chown -R fusionpbx:fusionpbx /etc/freeswitch
-fi
-
-if [ -d /var/cache/fusionpbx ]; then
-    chown -R fusionpbx:fusionpbx /var/cache/fusionpbx
-fi
-
-if [ -d /var/lib/freeswitch ]; then
-    chown -R fusionpbx:fusionpbx /var/lib/freeswitch
-fi
-
-if [ ! -d /etc/fusionpbx ]; then
-    mkdir -p /etc/fusionpbx
-    chown -R fusionpbx:fusionpbx /etc/fusionpbx
-fi
-
-if [ ! -d /dev/shm/freeswitch ]; then
-    mkdir -p /dev/shm/freeswitch
-fi
-
-chown fusionpbx:fusionpbx /dev/shm/freeswitch
-
 # Source docker-entrypoint.sh:
 # https://github.com/docker-library/postgres/blob/master/9.4/docker-entrypoint.sh
 # https://github.com/kovalyshyn/docker-freeswitch/blob/vanilla/docker-entrypoint.sh
