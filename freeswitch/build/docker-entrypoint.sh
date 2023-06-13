d46e7e3218c3d489635ee2441cb7fc5dba428adb
Tim Fry <tim.fry@hotmail.com>
2023-06-12 21:32:29 -0300

    always overwrite event socket modules

--- freeswitch/build/docker-entrypoint.sh
+++ freeswitch/build/docker-entrypoint.sh
@@ -11,6 +11,20 @@ mkdir -p /etc/fusionpbx
 mkdir -p /dev/shm/freeswitch
 chown -R fusionpbx:fusionpbx /etc/fusionpbx
 chown fusionpbx:fusionpbx /dev/shm/freeswitch
+#
+# overwrite the event_socket files
+#
+cat <<EOF > /etc/freeswitch/autoload_configs/event_socket.conf.xml
+<configuration name="event_socket.conf" description="Socket Client">
+        <settings>
+                <param name="nat-map" value="false" />
+                <param name="listen-ip" value="0.0.0.0" />
+                <param name="listen-port" value="8021" />
+                <param name="password" value="ClueCon" />
+                <param name="apply-inbound-acl" value="any_v4.auto" />
+        </settings>
+</configuration>
+EOF
 
 # Source docker-entrypoint.sh:
 # https://github.com/docker-library/postgres/blob/master/9.4/docker-entrypoint.sh
